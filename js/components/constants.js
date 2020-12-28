659cc37e7a56729625676c3c5de33b1042787506
web-mrAlderson <tima-999@inbox.ru>
2020-12-28 22:20:45 +0300

    fix: delite old file

--- js/components/constants.js
+++
@@ -1,5 +0,0 @@
-export const check = document.getElementById('check');
-export const preloader = document.querySelector('.preloader-wrapper');
-export const articel = document.getElementById('a');
-export const star = document.querySelector('.wrapper__star');
-export const gameOver = document.querySelector('#gameOver');
